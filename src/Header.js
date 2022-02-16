05f52048a952adb5deb8f6a40aa7da4968256760
minhyung_kim <minbrotherkim@gmail.com>
2022-02-16 14:49:03 +0900

    naver header 제작

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,22 @@
 import React from "react";
+import "./style/Header.css";
 
 function Header() {
   return (
-    <div>네이버로고, 검색창, 상단에는 시작페이지, 쥬니어네이버, 해피빈</div>
+    <div className="headerGroup">
+      <div className="header">
+        <div className="logo">
+          <img src={process.env.PUBLIC_URL + "/naverlogo.png"} />
+        </div>
+        <div className="searchArea">
+          <div className="searchBar">
+            <div>검색창</div>
+            <div>검색버튼</div>
+          </div>
+        </div>
+        <div className="text">네이버를 시작페이지로, 쥬니어네이버, 해피빈</div>
+      </div>
+    </div>
   );
 }
 
